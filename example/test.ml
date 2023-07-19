63c33bebc597b08a48942900eae0b477118a395e
public-release <as-public-release@janestreet.com>
2023-07-19 10:34:33 +0100

    v0.17~preview.128.30+99

--- example/test.ml
+++ example/test.ml
@@ -7,3 +7,8 @@ let _ =
 type 'a t  = X | F of 'a
 let _constr_on_left  f = X |> f
 let _constr_on_right x = x |> F
+
+let _constr_on_left_with_attr  f =
+  (X [@ppwarning "warning should be suppressed"] |> f) [@warning "-preprocessor"]
+let _constr_on_right_with_attr x =
+  (x [@ppwarning "warning should be suppressed"] |> F) [@warning "-preprocessor"]
