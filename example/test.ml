0581db74bc0d4d9fde22d258101e961265803fcb
public-release <as-public-release@janestreet.com>
2023-08-30 10:23:16 +0100

    v0.17~preview.128.37+01

--- example/test.ml
+++ example/test.ml
@@ -1,14 +1,19 @@
 let f x = x
 let g x = x
 let h x = x
-let _ =
-  (1 |> f) |> (g |> h)
+let _ = 1 |> f |> (g |> h)
 
-type 'a t  = X | F of 'a
-let _constr_on_left  f = X |> f
+type 'a t =
+  | X
+  | F of 'a
+
+let _constr_on_left f = X |> f
 let _constr_on_right x = x |> F
 
-let _constr_on_left_with_attr  f =
-  (X [@ppwarning "warning should be suppressed"] |> f) [@warning "-preprocessor"]
+let _constr_on_left_with_attr f =
+  ((X [@ppwarning "warning should be suppressed"]) |> f) [@warning "-preprocessor"]
+;;
+
 let _constr_on_right_with_attr x =
-  (x [@ppwarning "warning should be suppressed"] |> F) [@warning "-preprocessor"]
+  ((x [@ppwarning "warning should be suppressed"]) |> F) [@warning "-preprocessor"]
+;;
